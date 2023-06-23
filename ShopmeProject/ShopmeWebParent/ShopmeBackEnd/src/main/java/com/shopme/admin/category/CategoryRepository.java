99bd2fd137dada66d4a96c0d3f199dc160f730b8
ASUS <minhthoai190892@gmail.com>
2023-06-23 21:13:01 +0700

    73.-Exercise-Code-Update-Category-Function

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+
 
 import com.shopme.common.entity.Category;
 
-@Repository
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	/**
      * Hàm update enabled
