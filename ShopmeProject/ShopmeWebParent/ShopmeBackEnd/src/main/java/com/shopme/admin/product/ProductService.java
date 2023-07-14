92210f28d27c66b4d7ac73ba3ed475a6a7338ae7
ASUS <minhthoai190892@gmail.com>
2023-07-14 10:09:29 +0700

    107.-Exercise-Check-Uniqueness-of-Product

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
 
 @Service
@@ -17,24 +18,43 @@ public class ProductService {
 	public List<Product> listAll() {
 		return productRepository.findAll();
 	}
+
 	/**
 	 * hàm save product
+	 * 
 	 * @param product nhận một đối tượng product
 	 * @return trả về một product mới
 	 */
 	public Product saveProduct(Product product) {
-		
-		if (product.getId()==null) {
+
+		if (product.getId() == null) {
 			// product mới tạo thì set ngày tạo hiện tại
 			product.setCreatedTime(new Date());
 		}
-		if (product.getAlias()==null || product.getAlias().isEmpty()) {
-			String defaultAlias= product.getName().replaceAll(" ", "-");
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
 			product.setAlias(defaultAlias);
-		}else {
-			product.setAlias(product.getAlias().replaceAll(" " , "-"));
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
 		}
 		product.setUpdateTime(new Date());
 		return productRepository.save(product);
 	}
+
+	public String checkUnique(Integer id, String name) {
+
+		boolean isCreatingNew = (id == null || id == 0);
+		Product productByName = productRepository.findByName(name);
+		if (isCreatingNew) {
+			if (productByName != null) {
+				return "Duplicate";
+			}
+		} else {
+			if (productByName != null && productByName.getId() != null) {
+				return "Duplicate";
+			}
+		}
+		return "Ok";
+	}
+
 }
