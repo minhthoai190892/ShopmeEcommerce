81c63deb55dc3680b9fe5c8d5db83f4f79617927
ASUS <minhthoai190892@gmail.com>
2023-08-18 14:28:56 +0700

    164.-Exercise-Code-Customer-Login-and-Logout

--- ShopmeProject/ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,14 +17,30 @@ public class WebSecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		/*
 		http.authorizeHttpRequests(configurer -> configurer.anyRequest().permitAll()// không cần xét đăng nhập
 //                        .authenticated()//phải đăng nhập
-		).formLogin(login -> login// hiển thị form login
-				.permitAll());
+ * */
+		http.authorizeHttpRequests(configurer -> configurer.requestMatchers("/user").authenticated().anyRequest().permitAll()
+		).formLogin(login -> login
+				.loginPage("/login")// hiển thị form login
+				.usernameParameter("email")//sử dụng email để đăng nhập
+				.permitAll())
+		.logout(logout->logout.permitAll())
+		.rememberMe(me->me
+						  .key("3a46695e7e504b2e94c07d17f18d0366")
+						  .tokenValiditySeconds(14*24*60*60)
+				)
+		;
 		return http.build();
 
+	}
+	@Bean
+	 UserDetailsService userDetailsService() {
+		return new CustomerUserDetailsService();
 	}
 
 	/**
@@ -33,9 +50,7 @@ public class WebSecurityConfig {
 	 */
 	@Bean
 	WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring().requestMatchers("/css/**", "/images/**", "/js/**", "/fontawesome/**")
-
-		;
+		return web -> web.ignoring().requestMatchers("/css/**", "/images/**", "/js/**", "/fontawesome/**");
 	}
 
 }
